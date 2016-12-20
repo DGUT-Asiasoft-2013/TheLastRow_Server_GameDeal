da1b767762b58ddfd213d51e015aee56efbdcae5
AdwardA <528458430@qq.com>
2016-12-20 09:24:58 +0800

    a

--- src/main/java/com/cloudage/membercenter/service/IUserService.java
+++ src/main/java/com/cloudage/membercenter/service/IUserService.java
@@ -4,6 +4,7 @@ import com.cloudage.membercenter.entity.User;
 
 public interface IUserService {
 	User save(User user);
-	
-
+	User findByAccount(String account);
+	User findByEmail(String email);
+	User findById(Integer id);
 }
