db4363554de416336850c6cdd503e56258913cec
Administrator <Administrator@7A304-0029>
2017-01-11 08:52:52 +0800

    Revert "11"
    This reverts commit a9891b266d47e4ebb63546ac8fd3c8a15cff3b9c.

--- src/main/java/com/cloudage/membercenter/service/DefaultUserService.java
+++ src/main/java/com/cloudage/membercenter/service/DefaultUserService.java
@@ -17,9 +17,37 @@ public class DefaultUserService implements IUserService {
 
 	@Autowired
 	IUserRepository userRepo;
-
+	
 	@Override
-	public User save(User user) {
+	public User create(String account, String passwordHash) {
+		User user = new User();
+		user.setAccount(account);
+		user.setPasswordHash(passwordHash);
 		return userRepo.save(user);
 	}
+
+	@Override
+	public void login(String account, String passwordHash) {
+		
+
+	}
+
+	@Override
+	public User getCurrentUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean changePassword(String newPasswordHash) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void logout() {
+		// TODO Auto-generated method stub
+
+	}
+
 }
--- src/main/java/com/cloudage/membercenter/service/IUserService.java
+++ src/main/java/com/cloudage/membercenter/service/IUserService.java
@@ -3,7 +3,10 @@ package com.cloudage.membercenter.service;
 import com.cloudage.membercenter.entity.User;
 
 public interface IUserService {
-	User save(User user);
+	User create(String account, String passwordHash);
 	
-
+	void login(String account, String passwordHash);
+	User getCurrentUser();
+	boolean changePassword(String newPasswordHash);
+	void logout();
 }
